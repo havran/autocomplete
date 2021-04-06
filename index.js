bebdee8af299888f05f3f0811ba30ad8a6a49bca
Juraj Chlebec <juraj.chlebec@gmail.com>
2021-04-06 19:33:52 +0200

    feat: more changes

--- index.js
+++ index.js
@@ -2,16 +2,37 @@ const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
 
-fs.createReadStream('data/domains.txt')
+var domainCount = 0;
+var s = fs.createReadStream('data/domains.txt')
   .pipe(csv({
     skipComments: '--',
     separator: ';'
   }))
-  .on('data', (data) => results.push(data))
+  .on('data', (data) => {
+      results.push(data);
+
+      // Set top level domain.
+      tld = '.sk'
+      // Get 'domena' from data object, remove '.sk' from end.
+      sub = data['domena'].replace(/\.sk$/, '');
+
+      for (l = 1; l <= sub.length; l++) {
+          chunk = sub.substring(0, l)
+          // console.log(chunk);
+      }
+      chunkEnd = `${chunk}$`;
+
+      domainCount++;
+      // console.log(`${chunk}$`);
+
+      // Only 10 first domain
+      // if (domainCount === 10) {
+      //     s.destroy();
+      // }
+  })
   .on('end', () => {
-    console.log(results);
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
+      console.log(`>>> All ${domainCount} domains processed...`);
+  })
+  .on('close', () => {
+      console.log(`>>> ${domainCount} domains processed, stream destroyed...`);
   });
